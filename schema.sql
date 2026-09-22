2b857dbbce0a5b3e4b5eda644511bc92b8076a33
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-22 23:31:24 +0000

    Tue, Sep 22, 2026, 7:31 PM -04:00

--- schema.sql
+++ schema.sql
@@ -1,8 +1,25 @@
 --The players on my campaigns
 CREATE TABLE "players" (
+    "id" INTEGER,
+    "username" TEXT UNIQUE NOT NULL,
+    "first_name" TEXT,
+    "last_name" TEXT,
+    "joined_at" DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY("id")
+);
+
+--The characters of my players
+CREATE TABLE "playable_characters" (
+    "id" INTEGER,
+    "player_id" INTEGER,
+    "first_name" TEXT NOT NULL,
+    "middle_name" TEXT,
+    "last_name" TEXT,
+    PRIMARY KEY("id")
+    FOREIGN KEY("player_id") "players"."id"
+);
+
+--The non-playable characters of my campaigns
+CREATE TABLE "non_playable_characters" (
     "id"
-    "username"
-    "first_name"
-    "last_name"
-    "
 )
