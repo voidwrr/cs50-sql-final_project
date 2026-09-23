52a511f0630551429c2d6bd969f6b3ea2cd93666
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-23 18:25:25 +0000

    Wed, Sep 23, 2026, 2:25 PM -04:00

--- schema.sql
+++ schema.sql
@@ -1,3 +1,14 @@
 CREATE TABLE "ages" (
+    "id" INTEGER,
+    "name" TEXT NOT NULL,
+    "start_year" INTEGER NOT NULL,
+    "end_year" INTEGER,
+    "description" TEXT,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "campaigns" (
+    "id" INTEGER,
+    "title" TEXT NOT NULL,
     
 )
