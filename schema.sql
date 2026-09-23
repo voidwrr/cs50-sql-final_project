02eceac14bdd7b5a5635a9da3a6b78a2ecd94488
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-23 18:39:54 +0000

    Wed, Sep 23, 2026, 2:39 PM -04:00

--- schema.sql
+++ schema.sql
@@ -1,19 +1,49 @@
+--AGES OF THE WORLD
 CREATE TABLE "ages" (
     "id" INTEGER,
-    "name" TEXT NOT NULL,
+    "name" TEXT NOT NULL UNIQUE,
     "start_year" INTEGER NOT NULL,
     "end_year" INTEGER,
     "description" TEXT,
     PRIMARY KEY("id")
 );
 
+--GEOGRAPHY OF THE WORLD
+CREATE TABLE "continents" (
+    "id" INTEGER,
+    "name" TEXT UNIQUE NOT NULL,
+    "description" TEXT,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "regions" (
+    "id" INTEGER,
+    "continent_id" INTEGER NOT NULL,
+    "name" TEXT UNIQUE NOT NULL,
+    "description" TEXT,
+    "climate" TEXT,
+    "description" TEXT,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("continent_id") REFERENCES "continents"("id")
+)
+
+--CAMPAIGNS PLAYED
 CREATE TABLE "campaigns" (
     "id" INTEGER,
-    "title" TEXT NOT NULL,
+    "title" TEXT NOT NULL UNIQUE,
     "age_id" INTEGER,
     "description" TEXT,
     "start_date" DATE DEFAULT CURRENT_DATE NOT NULL,
     "end_date" DATE,
     PRIMARY KEY("id"),
     FOREIGN KEY("age_id") REFERENCES "ages"("id")
+);
+
+--PEOPLE THAT PLAYED
+CREATE TABLE "players" (
+    "id" INTEGER,
+    "username" TEXT UNIQUE NOT NULL,
+    "first_name" TEXT,
+    "last_name" TEXT,
+    "joined_at" DATE DEFAULT CURRENT_DATE NOT NULL,
 );
