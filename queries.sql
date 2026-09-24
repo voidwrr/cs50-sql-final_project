f1d09b9133adc11d1afd17f26e0d70ca25f5cb56
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-24 19:58:59 +0000

    Thu, Sep 24, 2026, 3:58 PM -04:00

--- queries.sql
+++ queries.sql
@@ -13,10 +13,16 @@ SELECT *
 FROM "v_character_sheet"
 WHERE "player" = 'voidwrr';
 
---The inventory from a certain character
+-- The inventory from a certain character
 SELECT "item_name", "item_type", "rarity", "requires_attunement"
 FROM "v_inventory_list"
-WHERE "entity_type" = 'PC' AND "owner_name" LIKE 'João%';
+WHERE "entity_type" = 'PC' AND "owner_name" LIKE 'Tytus%';
+
+-- The spellbook from a certain character
+SELECT "spell_name", "spell_level", "school", "casting_time", "range", "concentration"
+FROM "v_spell_list"
+WHERE "entity_type" = 'PC' AND "caster_name" LIKE 'Gandalf%'
+ORDER BY "spell_level" ASC, "spell_name" ASC;
 
 
 -- Complete npc sheet
@@ -28,3 +34,16 @@ WHERE "npc_name" LIKE '%Rurik%';
 SELECT *
 FROM "v_npc_sheet"
 WHERE "city" = 'Waterdeep';
+
+
+-- All cities and the climate from a certain region
+SELECT
+    c."name" AS "city_name",
+    c."type" AS "city_type",
+    r."name" AS "region_name",
+    r."climate" AS "climate",
+    cont."name" AS "continent"
+FROM "cities" AS c
+JOIN "regions" AS r ON c."region_id" = r."id"
+JOIN "continents" AS cont ON r."continent_id" = cont."id"
+WHERE cont."name" = 'The red desert';
