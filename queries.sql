684532ce9a2522e32a0221c3232e05ffc9359b98
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-24 20:08:59 +0000

    Thu, Sep 24, 2026, 4:08 PM -04:00

--- queries.sql
+++ queries.sql
@@ -3,6 +3,7 @@
 -- ============================================================================
 -- CHARACTERS AND PLAYERS QUERIES
 -- ============================================================================
+
 -- Complete character sheet
 SELECT *
 FROM "v_character_sheet"
@@ -12,6 +13,25 @@ WHERE "character_name" LIKE '%Rurik%';
 SELECT *
 FROM "v_character_sheet"
 WHERE "player" = 'voidwrr';
+
+-- Players and their characters of a certain campaing
+SELECT
+    "players"."username" "player_username",
+    "campaigns"."title" "campaign_title",
+    TRIM(
+        "chars"."first_name" ||
+        COALESCE(' ' || "chars"."middle_name", '') ||
+        COALESCE(' ' || "chars"."last_name", '')
+    ) "character_name",
+    "races"."name" "race",
+    "classes"."name" "primary_class"
+FROM "char_campaigns"
+JOIN "campaigns" ON "char_campaigns"."campaign_id" = "campaigns"."id"
+JOIN "chars" ON "char_campaigns"."char_id" = "chars"."id"
+JOIN "players" ON "chars"."player_id" = "players"."id"
+JOIN "races" ON "chars"."race_id" = "races"."id"
+JOIN "classes" ON "chars"."class" = "classes"."id"
+WHERE "campaigns"."id" = 1;
 
 -- The inventory from a certain character
 SELECT "item_name", "item_type", "rarity", "requires_attunement"
@@ -25,6 +45,10 @@ WHERE "entity_type" = 'PC' AND "caster_name" LIKE 'Gandalf%'
 ORDER BY "spell_level" ASC, "spell_name" ASC;
 
 
+-- ============================================================================
+-- NPCS QUERIES
+-- ============================================================================
+
 -- Complete npc sheet
 SELECT *
 FROM "v_npc_sheet"
@@ -35,6 +59,20 @@ SELECT "npc_name", "race", "primary_class", "factions"
 FROM "v_npc_sheet"
 WHERE "lived_at" = 'Waterdeep' OR "hometown" = 'Waterdeep';
 
+-- All npcs from a certain faction and their ranks
+SELECT
+    TRIM("npcs"."first_name" || COALESCE(' ' || "npcs"."middle_name", '') || COALESCE(' ' || "npcs"."last_name", '')) "npc_name",
+    "npc_factions"."rank" "faction_rank",
+    "factions"."name" "faction_name"
+FROM "npcs"
+JOIN "npc_factions" ON "npcs"."id" = "npc_factions"."npc_id"
+JOIN "factions" ON "npc_factions"."faction_id" = "factions"."id"
+WHERE "factions"."name" = 'Thieves Guild';
+
+
+-- ============================================================================
+-- LORE QUERIES
+-- ============================================================================
 
 -- All cities and the climate from a certain region
 SELECT
@@ -59,16 +97,10 @@ LEFT JOIN "ages" ages_end ON "factions"."end_age" = ages_end."id"
 WHERE ages_start."start_year" <= 1000
   AND (ages_end."end_year" IS NULL OR a_end."end_year" >= 1000);
 
--- All npcs from a certain faction and their ranks
-SELECT
-    TRIM("npcs"."first_name" || COALESCE(' ' || "npcs"."middle_name", '') || COALESCE(' ' || "npcs"."last_name", '')) "npc_name",
-    "npc_factions"."rank" "faction_rank",
-    "factions"."name" "faction_name"
-FROM "npcs"
-JOIN "npc_factions" ON "npcs"."id" = "npc_factions"."npc_id"
-JOIN "factions" ON "npc_factions"."faction_id" = "factions"."id"
-WHERE "factions"."name" = 'Thieves Guild';
 
+-- ============================================================================
+-- MECHANICAL QUERIES
+-- ============================================================================
 
 -- Spells from a certain level and school
 SELECT "name", "casting_time", "range", "concentration"
