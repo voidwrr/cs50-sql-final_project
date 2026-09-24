744ea079a6f932acc132299fe473e32ab9899f09
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-24 19:33:43 +0000

    Thu, Sep 24, 2026, 3:33 PM -04:00

--- queries.sql
+++ queries.sql
@@ -8,3 +8,8 @@
 SELECT *
 FROM "v_character_sheets"
 WHERE "character_name" LIKE '%Eldrin%';
+
+-- All characters from a certain player
+SELECT *
+FROM "v_character_sheets"
+WHERE "character_name" LIKE '%Eldrin%';
